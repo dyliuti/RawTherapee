434d16421b55ce4b4e1a7bd4a1b9fb955b19465f
yanminwei <yanminwei@baidu.com>
2026-03-31 21:19:50 +0800

    编译通过

--- tools/buildRT.bat
+++ tools/buildRT.bat
@@ -3,12 +3,16 @@ REM 2013-05-14 version 1
 
 SET RT_BUILD_TYPE=Release
 SET RT_CACHE_VER=4.0.11
-SET PATH=D:\app\code\msys64\mingw64\bin;%PATH%;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
-SET CC=D:\app\code\msys64\mingw64\bin\gcc.exe
-SET CXX=D:\app\code\msys64\mingw64\bin\g++.exe
+SET MSYS2_PATH=D:\app\code\msys64
+SET MSYSTEM=MINGW64
+SET CHERE_INVOKING=1
+SET PATH=%MSYS2_PATH%\mingw64\bin;%MSYS2_PATH%\usr\bin;%PATH%;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
+SET CC=%MSYS2_PATH%\mingw64\bin\gcc.exe
+SET CXX=%MSYS2_PATH%\mingw64\bin\g++.exe
 SET RT_SOURCECODE_PATH=%~dp0..
 FOR %%I IN ("%RT_SOURCECODE_PATH%") DO SET RT_SOURCECODE_PATH=%%~fI
 SET RT_BUILD_PATH=%RT_SOURCECODE_PATH%\build
+SET RT_OUTPUT_PATH=%RT_SOURCECODE_PATH%\output
 SET RT_SSE_SUPPORT=
 
 IF NOT DEFINED clean SET clean=n
@@ -49,6 +53,32 @@ GOTO compile
 
 :compile
 mingw32-make.exe "MAKE=mingw32-make -j%NUMBER_OF_PROCESSORS%" -j%NUMBER_OF_PROCESSORS% install
+IF ERRORLEVEL 1 (
+    ECHO Build failed
+    GOTO end
+)
+
+ECHO.
+ECHO Preparing output directory: "%RT_OUTPUT_PATH%"
+IF EXIST "%RT_OUTPUT_PATH%" rmdir /S /Q "%RT_OUTPUT_PATH%"
+mkdir "%RT_OUTPUT_PATH%"
+IF ERRORLEVEL 1 (
+    ECHO Failed to create output directory
+    GOTO end
+)
+
+IF NOT EXIST "%RT_BUILD_PATH%\%RT_BUILD_TYPE%\" (
+    ECHO Install output not found: "%RT_BUILD_PATH%\%RT_BUILD_TYPE%\"
+    GOTO end
+)
+
+ECHO Copying build artifacts to output directory...
+xcopy "%RT_BUILD_PATH%\%RT_BUILD_TYPE%\*" "%RT_OUTPUT_PATH%\" /E /I /Y >nul
+IF ERRORLEVEL 1 (
+    ECHO Failed to copy artifacts to output directory
+    GOTO end
+)
+ECHO Output ready at "%RT_OUTPUT_PATH%"
 GOTO end
 
 :end
